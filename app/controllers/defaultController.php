150a069c80b11ca84f440d1583e9f1ded332417f
yidemir <demiriy@gmail.com>
2016-12-02 12:25:39 +0200

    Update defaultController.php

--- app/controllers/defaultController.php
+++ app/controllers/defaultController.php
@@ -2,13 +2,28 @@
 
 class defaultController extends controller
 {
+	/**
+	 * ?url=default/index için aksiyon yazalım
+	 */
 	public function indexAction()
 	{
 		// Görünüm dosyasına gönderilecek değişkenler
 		// Görünüm dosyasında $title değişkenini kullanabileceğiz:
 		$data['title'] = 'Ana Sayfa';
+		$data['text'] = 'Ana sayfadan merhaba!';
 
 		// app/views/index.php görünümünü gösterelim
-		$this->render('index', $data); 
+		$this->render('index', $data);
+	}
+
+	/**
+	 * ?url=default/test için aksiyon yazalım
+	 */
+	public function testAction()
+	{
+		$data['title'] = 'Test Sayfası';
+		$data['text'] = 'Şu an test sayfasındasınız';
+
+		return $this->render('index', $data);
 	}
 }
