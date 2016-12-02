06752d38ae8278640f43d3ef8267c6325cd1d0e0
yidemir <demiriy@gmail.com>
2016-12-02 11:22:01 +0200

    Create post.php

---
+++ app/models/post.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Örnek bir model dosyası
+ * post tablosunu temsil edecek
+ */
+class post extends model
+{
+	/**
+	 * Bütün gönderileri (postları) getirmesini sağlayalım
+	 * $this->fetchAll'ı genişlettiğimiz (extend) model sınıfı aracılığıyla kullanıyoruz
+	 * @return array
+	 */
+	public function getAll()
+	{
+		return $this->fetchAll('SELECT * FROM post ORDER BY created DESC');
+	}
+}
