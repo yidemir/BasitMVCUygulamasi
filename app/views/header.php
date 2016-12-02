4fcca8f1ea9e4be74d7cb91aa3477f2a1d9fd843
yidemir <demiriy@gmail.com>
2016-12-02 11:28:20 +0200

    Create header.php

---
+++ app/views/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo isset($title) ? $title : 'MVC Uygulaması' ?></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container mt-3">
