f08b8a7d1a2ace7d41b901a3dc8e2a5a4806cd12
yidemir <demiriy@gmail.com>
2016-12-02 11:32:39 +0200

    Create index.php

---
+++ app/views/index.php
@@ -0,0 +1,6 @@
+<?php require VDIR.'/header.php' ?>
+
+<?php if (isset($title)): ?><h1><?php echo $title ?></h1><?php endif ?>
+<p>Ana sayfaya hoş geldiniz!</p>
+
+<?php require VDIR.'/footer.php' ?>
